b693e63d2e10ee8f1834199b824afe0d2cb8bd06
Kyle Coberly <kyle.coberly@gmail.com>
2017-10-10 15:28:38 -0600

    Updating files

--- drills/version-1/app/index.js
+++ drills/version-1/app/index.js
@@ -1,28 +1,4 @@
-var register = {
-    transactions: []
-}
-
-function addTransaction(transaction){
-    register.transactions.push(transaction);
-}
-
-function getTotal(){
-    var total = 0;
-    for (let i = 0; i < register.transactions.length; i++){
-        for (let j = 0; j < register.transactions[i].items.length; j++){
-            total += register.transactions[i].items[j].price * register.transactions[i].items[j].quantity;
-        }
-    }
-    return total;
-}
-
-function getFormattedTotal(){
-    return getTotal().toFixed(2);
-}
-
-function reset(){
-    register.transactions = [];
-}
+// Your code here
 
 module.exports = {
     register,
