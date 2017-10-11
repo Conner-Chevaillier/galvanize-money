f7fcac84253573c36a5a470b81a0381270933e1c
Kyle Coberly <kyle.coberly@gmail.com>
2017-10-11 10:45:17 -0600

    Removing solution

--- drills/version-2/app/index.js
+++ drills/version-2/app/index.js
@@ -1,43 +1,10 @@
-var register = {
-    transactions: []
-};
-
-function addTransaction(transaction){
-    register.transactions.push(transaction);
-}
-
-function reset(){
-    register.transactions = [];
-}
-
-function countSalesOfType(type){
-    var count = 0;
-    for (let i = 0; i < register.transactions.length; i++){
-        for (let j = 0; j < register.transactions[i].items.length; j++){
-            var item = register.transactions[i].items[j];
-            if (item.description === type){
-                count += item.quantity;
-            }
-        }
-    }
-    return count;
-}
-
-function getTransactionsBetween(firstDate, secondDate){
-    var transactions = [];
-    for (let i = 0; i < register.transactions.length; i++){
-        let currentTransaction = register.transactions[i];
-        if (currentTransaction.date > new Date(firstDate) && currentTransaction.date < new Date(secondDate)){
-            transactions.push(currentTransaction);
-        }
-    }
-    return transactions;
-}
+// Your code here
 
 module.exports = {
-    register,
-    addTransaction,
-    countSalesOfType,
-    getTransactionsBetween,
-    reset,
+    // Uncomment these as you define them
+    // register,
+    // addTransaction,
+    // countSalesOfType,
+    // getTransactionsBetween,
+    // reset,
 };
