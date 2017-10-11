759a43915348d5407d3feeff7994bc2604f7621a
Kyle Coberly <kyle.coberly@gmail.com>
2017-10-11 11:33:35 -0600

    Removing solution

--- drills/version-3/app/index.js
+++ drills/version-3/app/index.js
@@ -1,34 +1,10 @@
-var register = {
-    transactions: []
-};
-
-function addTransaction(transaction){
-    register.transactions.push(transaction);
-}
-
-function getTransactionsBySalesperson(salesperson){
-    var transactions = [];
-    for (let i = 0; i < register.transactions.length; i++){
-        let currentTransaction = register.transactions[i];
-        if (currentTransaction.cashier === salesperson){
-            transactions.push(currentTransaction);
-        }
-    }
-    return transactions;
-}
-
-function removeLastTransaction(){
-    return register.transactions.pop();
-}
-
-function reset(){
-    register.transactions = [];
-}
+// Your code here
 
 module.exports = {
-    register,
-    addTransaction,
-    getTransactionsBySalesperson,
-    removeLastTransaction,
-    reset
+    // Uncomment these as you define them
+    // register,
+    // addTransaction,
+    // getTransactionsBySalesperson,
+    // removeLastTransaction,
+    // reset
 };
