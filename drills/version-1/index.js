41cb861c7705f09d901e057d0f9451b75cd7b8eb
Uvizius <connerchevaillier@gmail.com>
2018-09-18 08:48:51 -0600

    here is galvanize money

--- drills/version-1/index.js
+++ drills/version-1/index.js
@@ -1,10 +1,38 @@
-// Your code here
+var register = {
+    transactions: []
+}
+
+function addTransaction(transaction) {
+    register.transactions.push(transaction)
+}
+
+function reset() {
+    register.transactions = []
+}
+
+function getTotal() {
+    let total = 0;
+    for (var i = 0; i < register.transactions.length; i++) {
+        for (var j = 0; j < register.transactions[i].items.length; j++) {
+            total += register.transactions[i].items[j].price * register.transactions[i].items[j].quantity
+        }
+    }
+    return total
+}
+
+function getFormattedTotal() {
+    return getTotal().toFixed(2);
+}
+
+
+
+
 
 module.exports = {
     // Uncomment these as you define them
-    // register,
-    // addTransaction,
-    // getTotal,
-    // getFormattedTotal,
-    // reset,
-};
+    register,
+    addTransaction,
+    getTotal,
+    getFormattedTotal,
+    reset,
+}
